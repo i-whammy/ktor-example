01af3e977cea5502bd5ea825f6a6c25839a24de3
i-whammy <yas.iwami@gmail.com>
2024-11-02 22:08:25 +0900

    first commit

---
+++ main/kotlin/com/example/Application.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.jetty.jakarta.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
