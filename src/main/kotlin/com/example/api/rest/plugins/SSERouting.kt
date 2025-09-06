7ab11c2a37409e750a088ff7487b968397c5cef5
i-whammy <yas.iwami@gmail.com>
2025-09-06 21:48:58 +0900

    SSE example

--- src/main/kotlin/com/example/api/rest/plugins/SSERouting.kt
+++ src/main/kotlin/com/example/api/rest/plugins/SSERouting.kt
@@ -1,18 +1,38 @@
 package com.example.api.rest.plugins
 
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.sse.*
-import io.ktor.sse.*
-import kotlin.time.Duration.Companion.milliseconds
+import io.ktor.sse.ServerSentEvent
+import io.ktor.utils.io.*
+import kotlinx.coroutines.delay
+import kotlinx.io.readLine
 
 fun Application.configureSSERouting() {
     install(SSE)
     routing {
         sse("/events") {
-            heartbeat {
-                period = 500.milliseconds
-                event = ServerSentEvent("heart beating")
+            val client = HttpClient(CIO)
+            client.prepareGet("http://localhost:8081/sse") {
+                method = HttpMethod.Get
+                headers {
+                    append("Accept", "text/event-stream")
+                }
+            }.execute { response ->
+                val channel: ByteReadChannel = response.body()
+                while (!channel.exhausted()) {
+                    var line: String?
+                    val buffer = channel.readBuffer()
+                    while (buffer.readLine().also { line = it } != null) {
+                        delay(500)
+                        send(ServerSentEvent(line))
+                    }
+                }
             }
         }
     }
--- src/main/kotlin/com/example/mock/WireMockRunner.kt
+++ src/main/kotlin/com/example/mock/WireMockRunner.kt
@@ -1,32 +1,42 @@
 package com.example.mock
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import com.github.tomakehurst.wiremock.client.WireMock.aResponse
-import com.github.tomakehurst.wiremock.client.WireMock.get
-import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.*
 import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 object WireMockRunner {
     private val logger = LoggerFactory.getLogger(this::class.java)
     private val wireMockServer = WireMockServer(8081)
     fun run() {
         wireMockServer.start()
-        wireMockServer.stubFor(get(urlEqualTo("/sse"))
-            .willReturn(aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "text/event-stream")
-                .withHeader("Cache-Control", "no-cache")
-                .withHeader("Connection", "keep-alive")
-                .withBody(
-                    """
-                    data: Message 1
-
-                    data: Message 2
-
-                    data: Message 3
-
-                    """
-                )))
+        wireMockServer.stubFor(
+            get(urlEqualTo("/sse"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/event-stream")
+                        .withHeader("Cache-Control", "no-cache")
+                        .withHeader("Connection", "keep-alive")
+                        .withBody(createEvents(100))
+                )
+        )
         logger.info("WireMock server started.")
     }
 }
+
+private fun createEvents(time: Int): String {
+    val builder = StringBuilder()
+    repeat(time) { n ->
+        builder.append("data: ${Event("Event message $n")}")
+        builder.append("\n")
+    }
+    return builder.toString()
+}
+
+data class Event(
+    val content: String, val created: Long = LocalDateTime.now().toInstant(
+        ZoneOffset.UTC
+    ).toEpochMilli()
+)
